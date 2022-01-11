7b7cad04409fcf6153ddc97546af85a2a4b56c5f
Talip <talipakcelik@hacettepe.edu.tr>
2022-01-11 22:00:03 +0300

    loopBooks, newCard and addBookToLibrary functios are added

--- script.js
+++ script.js
@@ -2,7 +2,13 @@
 
 //// DOM
 const textArea = document.querySelectorAll("textarea");
-const button = document.querySelector("button");
+const textAreaTitle = document.getElementById("title");
+const textAreaAuthor = document.getElementById("author");
+const textAreaPage = document.getElementById("page");
+const buttonSubmit = document.querySelector(".submit");
+const buttonNew = document.querySelector(".new");
+const library = document.querySelector(".books-container");
+const catalogContainer = document.querySelector(".books-container");
 
 let myLibrary = [];
 
@@ -10,13 +16,39 @@ function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
   this.pages = pages;
-  this.read = read;
-  this.info = function () {
-    console.log(`${this.title} by ${this.author}, ${this.pages}, ${this.read}`);
-  };
-  return console.log(this.info());
+  // this.read = read;
 }
 
-function addBookToLibrary() {}
+function addBookToLibrary() {
+  const title = textAreaTitle.value;
+  const author = textAreaAuthor.value;
+  const page = Number(textAreaPage.value);
+
+  const book1 = new Book(title, author, page);
+  console.log(book1);
+  myLibrary.push(book1);
+}
 
 const pusu = new Book("Pusu", "Janka", "215", "okundu");
+
+buttonSubmit.addEventListener("click", function () {
+  addBookToLibrary();
+  loopBooks();
+  newCard();
+});
+
+let catalog = "";
+
+const loopBooks = function () {
+  const values = Object.values(myLibrary);
+
+  for (const { title, author, pages } of values) {
+    catalog = `${title} ${author} ${pages}`;
+  }
+};
+
+const newCard = function () {
+  const div = document.createElement("div");
+  catalogContainer.appendChild(div);
+  div.textContent = catalog;
+};
